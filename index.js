06fdbc4c91a367364adc836eecbb29afd4bfe84b
tmaa <thomasaanum@gmail.com>
2022-02-06 14:29:47 -0500

    added validation

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express');
+const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config();
 
+const port = process.env.PORT || 3001
+
 //db connection
-mongoose.connect(
-  process.env.MONGODB_CONNECTION, 
+mongoose.connect(process.env.MONGODB_CONNECTION, 
   () => console.log("Connected to db")
 );
 
-const app = express();
+//middleware
 app.use(express.json());
-
 app.use(cors());
-
-const port = process.env.PORT || 3001
 
 //import routes
 const authRoutes = require("./routes/auth")
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,41 @@
-const express = require("express")
+const express = require('express')
+const User = require('../models/User');
 
 const router = express.Router();
 
-//POST - Create new user
-router.post("/register", (req,res,next) => {
-  console.log(req.body)
+//validation
+const Joi = require('@hapi/joi');
 
-  res.send({message: `User created`})
+const schema = Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().max(50).required(),
+  age: Joi.number().required(),
+  height: Joi.number().required(),
+  email: Joi.string().required().email(),
+});
+
+//POST - Create new user
+router.post("/register", async (req,res,next) => {
+  //validate before db entry
+  const validation = schema.validate(req.body)
+
+  console.log(validation)
+  const user = new User({
+    _id: req.body.id,
+    name: req.body.name,
+    age: req.body.age,
+    height: req.body.height,
+    email: req.body.email
+  });
+
+  try{
+    const savedUser = await user.save()
+    res.send(savedUser);
+  }catch(err){
+    console.log(err)
+    res.status(400).send(err)
+  }
+
   next();
 });
 
@@ -21,8 +50,10 @@ router.put("/api/users/:id", (req, res, next) => {
 });
 
 //DELETE - Delete specific user
-router.delete("/api/users/:id", (req, res, next) => {
-
+router.delete("/delete/:id", (req, res, next) => {
+  console.log("user deleted: " + req.params.id)
+  res.send({message: `User deleted`})
+  next();
 });
 
 module.exports = router
